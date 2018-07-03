2a00dad44286fda6f90a8c2a1691ae2aed22b9f5
crystalcamarao <40783005+crystalcamarao@users.noreply.github.com>
2018-07-03 17:49:28 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+Time Until Christmas
+===================================
+
+A simple Twitter *Christmas* countdown bot
+
+[Learn more](https://medium.com/@crystalcamarao/how-to-develop-a-simple-twitter-countdown-bot-7a1757f5c2c3)
+
+
+**Powered by [Glitch](https://glitch.com)**
+
+\ ゜o゜)ノ
---
+++ server.js
@@ -0,0 +1,117 @@
+/* Setting things up. */
+var path = require('path'),
+    express = require('express'),
+    app = express(),   
+    Twit = require('twit'),
+    config = {
+    /* Be sure to update the .env file with your API keys. See how to get them: https://botwiki.org/tutorials/how-to-create-a-twitter-app */      
+      twitter: {
+        consumer_key: process.env.CONSUMER_KEY,
+        consumer_secret: process.env.CONSUMER_SECRET,
+        access_token: process.env.ACCESS_TOKEN,
+        access_token_secret: process.env.ACCESS_TOKEN_SECRET
+      }
+    },
+    T = new Twit(config.twitter);
+
+app.use(express.static('public'));
+
+app.all("/" + process.env.BOT_ENDPOINT, function (req, res) {
+  
+  T.post('statuses/update', { status: countdownMessage() }, function(err, data, response) {
+    if (err){
+      console.log('error!', err);
+      res.sendStatus(500);
+    }
+    else{
+      res.sendStatus(200);
+    }
+  });
+});
+
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your bot is running on port ' + listener.address().port);
+});
+
+const moment = require ("moment");
+
+const current = new Date();
+
+var currentYear = current.getFullYear();
+
+//For December 26-31, set to NEXT year. You can remove this entire code block!
+var currentMonth = current.getMonth();
+var currentDate = current.getDate();
+if(currentMonth==11)
+   {
+     if (currentDate==26 || currentDate==27 || currentDate==28 || currentDate==29 || currentDate==30 || currentDate==31)
+       currentYear = currentYear + 1;
+   }
+
+const now = new Date().getTime();
+
+//Set the date to which you want to count down to here!
+const end =  new Date(currentYear, 11, 25, 0, 0, 0, 1).getTime();
+
+const diffTime = end - now;
+
+var diffDuration = moment.duration(diffTime, 'milliseconds');
+
+/* Returns the message to send as our status update */
+function countdownMessage() {
+  
+  var status = '';
+  
+   if(diffTime > 0) 
+{
+
+     status += 'Only';
+
+     if(diffDuration.months() > 1 )
+     {
+			status += ` ${diffDuration.months()} months`;
+     }
+     else if (diffDuration.months() == 1 )
+     {
+			status += ` ${diffDuration.months()} month`;
+   	 }
+   	
+	if(diffDuration.days() > 1 )
+     {
+			status += ` ${diffDuration.days()} days`;
+     }
+     else if (diffDuration.days() == 1 )
+     {
+			status += ` ${diffDuration.days()} day`;
+   	 }
+    
+    if(diffDuration.hours() > 1 )
+     {
+			status += ` ${diffDuration.hours()} hours`;
+     }
+     else if (diffDuration.hours() == 1 )
+     {
+			status += ` ${diffDuration.hours()} hour`;
+   	 }
+	
+	if(diffDuration.minutes() > 1 )
+     {
+			status += ` ${diffDuration.minutes()} minutes`;
+     }
+     else if (diffDuration.minutes() == 1 )
+     {
+			status += ` ${diffDuration.minutes()} minute`;
+   	 }
+	
+//Change the text about Christmas!
+	status += ' left until #Christmas' + currentYear + '! #Christmas #ChristmasCountdown #Xmas #ChristmasTime #holidays #MerryChristmas';
+}
+else
+{
+    //Change the text about Christmas!
+    status += 'It\'s finally Christmas! MERRY CHRISTMAS EVERYONE!';
+}
+  
+  return status;
+  
+};
